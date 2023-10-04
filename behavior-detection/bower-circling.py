09fcd29a70af8f7afd647eda35fbeae3f841ca62
michael <45michaelo88@gmail.com>
2023-10-04 10:57:14 -0400

    Update approximations to include centre mass

--- behavior-detection/bower-circling.py
+++ behavior-detection/bower-circling.py
@@ -2,8 +2,6 @@ import os
 import pickle
 import pandas as pd
 import numpy as np
-import cv2
-
 
 filepath_pickle = r"/home/bree_student/Downloads/dlc_model-student-2023-07-26/videos/MC_singlenuc26_2_Tk63_022520/bower_circling/bower_circlingDLC_dlcrnetms5_dlc_modelJul26shuffle4_100000_assemblies.pickle"
 video = r"/home/bree_student/Downloads/dlc_model-student-2023-07-26/videos/MC_singlenuc26_2_Tk63_022520/bower_circling/bower_circlingDLC_dlcrnetms5_dlc_modelJul26shuffle4_100000_el_filtered_id_labeled.mp4"
@@ -11,18 +9,21 @@ out = r"/home/bree_student/Downloads/dlc_model-student-2023-07-26/videos/MC_sing
 
 data_pickle = pd.read_pickle(filepath_pickle)
 
+
 def read_video(video: str, output: str):
+    import cv2
     vid = cv2.VideoCapture(video)
     success, image = vid.read()
     count = 0
     while success:
-      cv2.imwrite(f"{os.path.join(output, f'frame{count}.png')}", image)
-      success,image = vid.read()
-      print('Read a new frame: ', success)
-      count += 1
+        cv2.imwrite(f"{os.path.join(output, f'frame{count}.png')}", image)
+        success, image = vid.read()
+        print('Read a new frame: ', success)
+        count += 1
 
 
 def show_nframes(frames: str, n: int):
+    import cv2
     for i in range(n):
         image = cv2.imread(f"{os.path.join(frames, f'frame{i}.png')}")
         window_name = f'frame{i}'
@@ -31,6 +32,7 @@ def show_nframes(frames: str, n: int):
         cv2.destroyWindow(window_name)
     cv2.destroyAllWindows()
 
+
 def get_centroid(xy_coords: list[tuple]):
     x_sum, y_sum = 0, 0
     for i in xy_coords:
@@ -38,11 +40,23 @@ def get_centroid(xy_coords: list[tuple]):
         y_sum += i[1]
     return (x_sum / len(xy_coords), y_sum / len(xy_coords))
 
+
+frames = data_pickle[70:90]
 frame = data_pickle[70]
-for individual in frame:
-    # x-y tuples for nose, lefteye, righteye, and spine1
-    # the centre of this cluster is our approximation for head location
-    front_cluster = [(individual[i][0], individual[i][1]) for i in range(4)]
-    head = get_centroid(front_cluster)
-    tail = (individual[-1][0], individual[-1][1])
-    print(head, tail)
+
+for i in range(1, len(frames)):
+    prev_frame, curr_frame = data_pickle[i - 1], data_pickle[i]
+
+
+def get_appoximations(frame):
+    for individual in frame:
+        # front cluster is the centroid of nose, lefteye, righteye, and spine1
+        # middle cluster is the centroid of spine2, spine3, leftfin, and rightfin
+        # tail is the backfin
+        # these approximations help abstract the fish and its movement
+        front_cluster = [(individual[i][0], individual[i][1]) for i in range(4)]
+        middle_cluster = [(individual[i][0], individual[i][1]) for i in range(3, 9) if i != 6]
+        head = get_centroid(front_cluster)
+        centre = get_centroid(middle_cluster)
+        tail = (individual[6][0], individual[6][1])
+        return (head, centre, tail)
